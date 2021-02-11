6f60cda39026f9442f47e9ea3145fc75ca022a5f
Jiwon Chang <hello@jwpedia.com>
2021-02-12 01:16:59 +0900

    Switched to new AmplifyAuthenticator

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Auth } from 'aws-amplify'
-
-function checkUser() {
-  Auth.currentAuthenticatedUser()
-  .then(user => console.log({user}))
-  .catch(err => console.log(err))
-}
-
-function signOut() {
-  Auth.signOut()
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
-}
+import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn, AmplifySignUp } from '@aws-amplify/ui-react';
+import config from './aws-exports';
 
 function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Signin Demo</h1>
-        <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Sign in with Google</button>
-        <button onClick={() => Auth.federatedSignIn({provider: 'LoginWithAmazon'})}>Sign in with Amazon</button>
-        <button onClick={checkUser}>Username: {checkUser}</button>
-        <button onClick={signOut}>Sign Out</button>
+        <AmplifyAuthenticator>
+          <AmplifySignIn headerText="Sign In" slot="sign-in"/>
+          <AmplifySignUp headerText="Sign Up" slot="sign-up"/>
+          <div>
+            My App
+            <AmplifySignOut />
+          </div>
+        </AmplifyAuthenticator>
       </header>
     </div>
   );
