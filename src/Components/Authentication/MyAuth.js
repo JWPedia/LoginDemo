f3bf57cf7d56c4b8ea61f34ac2c74b1570913f71
Jiwon Chang <hello@jwpedia.com>
2021-02-12 04:25:54 +0900

    Using newer authentication API

--- src/Components/Authentication/MyAuth.js
+++ src/Components/Authentication/MyAuth.js
@@ -1,15 +1,22 @@
 import { Component } from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn, AmplifySignUp } from '@aws-amplify/ui-react';
 import './MyAuth.css'
 
-class MyAuth extends Component {
-    render() {
-        return (
-          <div className = "AuthWrapper">
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AmplifyAuthenticator>
+          <AmplifySignIn headerText="Sign In" slot="sign-in"/>
+          <AmplifySignUp headerText="Sign Up" slot="sign-up"/>
+          <div>
+            My App
             <AmplifySignOut />
           </div>
-        );
-    }
+        </AmplifyAuthenticator>
+      </header>
+    </div>
+  );
 }
 
-export default withAuthenticator(MyAuth);
+export default App;
