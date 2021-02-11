12f5052a0da59619dbf4b2fc34f29883de8d5f4a
Jiwon Chang <hello@jwpedia.com>
2021-02-12 00:55:24 +0900

    Added inddividual singin and signout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 import { Auth } from 'aws-amplify'
 
-function App() {
+function checkUser() {
+  Auth.currentAuthenticatedUser()
+  .then(user => console.log({user}))
+  .catch(err => console.log(err))
+}
+
+function signOut() {
+  Auth.signOut()
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
+}
+
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, world!</h1>
-        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        <h1>Signin Demo</h1>
+        <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Sign in with Google</button>
+        <button onClick={() => Auth.federatedSignIn({provider: 'LoginWithAmazon'})}>Sign in with Amazon</button>
+        <button onClick={checkUser}>Username: {checkUser}</button>
+        <button onClick={signOut}>Sign Out</button>
       </header>
     </div>
   );
